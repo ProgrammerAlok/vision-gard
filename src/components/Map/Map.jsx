a74861f64dc4b4676361da07b38094cebb7eaa94
ProgrammerAlok <alokpaul5584@gmail.com>
2023-09-05 19:09:58 +0530

    map component added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
-import "./map.css";
-import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./map.css";
+import "leaflet/dist/leaflet.css";
 
 const Map = () => {
   const [zoom, setZoom] = useState(13);
@@ -9,38 +9,9 @@ const Map = () => {
     Lat: 22.66247363997702,
     Long: 88.44767905068652,
   });
-  // const map = L.map('map', { center: [center[0], center[1]], zoom: zoom})
-
-//   useEffect(() => {
-//     navigator.geolocation.getCurrentPosition(
-//       (position) => {
-//         console.log(position);
-//         setCenter({
-//           Lat: position.coords.latitude,
-//           Long: position.coords.longitude,
-//         });
-//         // map.flyTo({lat: center.Lat, lng: center.Long}, 13)
-//       },
-//       (err) => {
-//         console.log(err);
-//       }
-//     );
-//   }, []);
 
   return (
     <div className="map-container" >
-      <input
-        type="number"
-        placeholder="Latitude"
-        value={center.Lat}
-        onChange={(e) => setCenter({ ...center, Lat: e.target.value })}
-      />
-      <input
-        type="number"
-        placeholder="Longitude"
-        value={center.Long}
-        onChange={(e) => setCenter({ ...center, Long: e.target.value })}
-      />
       <MapContainer
         center={[center.Lat, center.Long]}
         zoom={zoom}
